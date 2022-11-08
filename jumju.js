38dd9cab24d47111edf9d7e0eae8f11a959e776b
Yeun1104 <bliass9050@gmail.com>
2022-11-09 08:43:06 +0900

    점주페이지 js

---
+++ jumju.js
@@ -0,0 +1,12 @@
+
+function setThumbnail(event) {
+    var reader = new FileReader();
+    
+    reader.onload = function(event) {
+    var img = document.createElement("img");
+    img.setAttribute("src", event.target.result);
+    document.querySelector("div#image_container").appendChild(img);
+    };
+    
+    reader.readAsDataURL(event.target.files[0]);
+}
